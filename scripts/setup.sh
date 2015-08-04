32e6b4fba06f169d600006b8174361b709112e22
Stan Bondi <stan@fixate.it>
2015-08-04 18:19:36 +0200

    Imagemagick

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,8 +5,10 @@ set -e
 # Add app user
 adduser --disabled-password --home=/app --gecos "" appuser
 
+apt-get update
+
 # Packages needed to build Ruby
-DEPS='autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev graphicsmagick'
+DEPS='autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev imagemagick'
 apt-get install -y $DEPS
 
 # Clean up
