f5549c41e148bcd9605a811eac231b5eca2d92c7
Stan Bondi <stan@fixate.it>
2015-09-21 13:48:05 +0200

    Yet another try at installing node

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -3,17 +3,11 @@
 set -eo pipefail
 
 cd /tmp
-NODE_DIST=${NODE_DIST:-node-latest}
-wget http://nodejs.org/dist/${NODE_DIST}.tar.gz -O node.tar.gz
-tar xvzf node.tar.gz
-rm -f node.tar.gz
-cd node-v*
-./configure
-CXX="g++ -Wno-unused-local-typedefs" make
-CXX="g++ -Wno-unused-local-typedefs" make install
-cd /tmp
-rm -rf /tmp/node-v*
-printf '\n# Node.js\nexport PATH="node_modules/.bin:$PATH"' >> /root/.bashrc
-export PATH="node_modules/.bin:$PATH"
-npm install -g npm
 
+wget https://deb.nodesource.com/node_0.12/pool/main/n/nodejs/nodejs_0.12.7-1nodesource1~trusty1_amd64.deb -O node.deb
+dpkg -i node.deb
+rm -f node.deb
+
+npm install -g node-gyp
+npm cache clear
+node-gyp configure
