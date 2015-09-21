b91e7cbfd5fd61e6dac27f31e0af57af1618e058
Stan Bondi <stan@fixate.it>
2015-09-21 14:01:24 +0200

    rlwrap required

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,7 +8,7 @@ adduser --disabled-password --home=/app --gecos "" appuser
 apt-get update
 
 # Packages needed to build Ruby
-DEPS='autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev imagemagick'
+DEPS='autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev imagemagick rlwrap'
 apt-get install -y $DEPS
 
 # Clean up
